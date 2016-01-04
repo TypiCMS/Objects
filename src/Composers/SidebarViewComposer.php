3798e8ab59cf714f803151e010a95ff349efffb7
Samuel De Backer <sdebacker@gmail.com>
2016-01-04 23:18:47 +0100

    config

--- src/Composers/SidebarViewComposer.php
+++ src/Composers/SidebarViewComposer.php
@@ -13,7 +13,7 @@ class SidebarViewComposer extends BaseSidebarViewComposer
     {
         $view->sidebar->group(trans('global.menus.content'), function (SidebarGroup $group) {
             $group->addItem(trans('objects::global.name'), function (SidebarItem $item) {
-                $item->icon = config('typicms.objects.sidebar.icon', 'icon fa fa-fw fa-cubes');
+                $item->icon = config('typicms.objects.sidebar.icon');
                 $item->weight = config('typicms.objects.sidebar.weight');
                 $item->route('admin.objects.index');
                 $item->append('admin.objects.create');
