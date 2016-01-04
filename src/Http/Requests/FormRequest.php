d4e0278a42c10ffb7ea432ed5d65b8d417ea96db
Samuel De Backer <sdebacker@gmail.com>
2016-01-04 23:18:38 +0100

    validation rules

--- src/Http/Requests/FormRequest.php
+++ src/Http/Requests/FormRequest.php
@@ -9,11 +9,11 @@ class FormRequest extends AbstractFormRequest
     public function rules()
     {
         $rules = [
-            'position' => 'required|integer|min:1',
-            'image'    => 'image|max:2000',
+            'image' => 'image|max:2000',
         ];
         foreach (config('translatable.locales') as $locale) {
-            $rules[$locale.'.website'] = 'url';
+            $rules[$locale.'.title'] = 'max:255';
+            $rules[$locale.'.slug'] = 'max:255';
         }
 
         return $rules;
