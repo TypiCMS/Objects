36099f5745f951f21861c6dcc85d5f753c2fd9c1
Rafael Perelló Rowley <rperellorow@gmail.com>
2016-03-16 11:54:09 +0100

    Added translation to spanish

---
+++ src/resources/lang/es/global.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'name'     => 'Objetos',
+    'objects'  => 'objeto|objetos',
+    'New'      => 'Nuevo objeto',
+    'Edit'     => 'Editar objeto',
+    'Back'     => 'Volver a los objetos',
+];
