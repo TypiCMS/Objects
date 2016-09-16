9f8c5d15349af43677ae3c35e57ce9a90dda3315
Samuel De Backer <sdebacker@gmail.com>
2016-09-16 23:39:24 +0200

    missing use

--- src/Repositories/EloquentObject.php
+++ src/Repositories/EloquentObject.php
@@ -2,6 +2,7 @@
 
 namespace TypiCMS\Modules\Objects\Repositories;
 
+use TypiCMS\Modules\Core\EloquentRepository;
 use TypiCMS\Modules\Objects\Models\Object;
 
 class EloquentObject extends EloquentRepository
