a386c8b79893057d6e9c693b29f90b53533ea071
Samuel De Backer <sdebacker@gmail.com>
2017-01-20 18:22:38 +0100

    validations attributes in a json file

--- src/resources/views/admin/_form.blade.php
+++ src/resources/views/admin/_form.blade.php
@@ -10,6 +10,6 @@
 
 @include('core::form._title-and-slug')
 {!! TranslatableBootForm::hidden('status')->value(0) !!}
-{!! TranslatableBootForm::checkbox(__('validation.attributes.online'), 'status') !!}
-{!! TranslatableBootForm::textarea(__('validation.attributes.summary'), 'summary')->rows(4) !!}
-{!! TranslatableBootForm::textarea(__('validation.attributes.body'), 'body')->addClass('ckeditor') !!}
+{!! TranslatableBootForm::checkbox(__('Online'), 'status') !!}
+{!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
+{!! TranslatableBootForm::textarea(__('Body'), 'body')->addClass('ckeditor') !!}
