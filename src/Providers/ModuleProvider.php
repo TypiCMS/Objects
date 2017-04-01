b084a77394ee33cc5f08b3dd3ca3b180dc2d3f88
Samuel De Backer <sdebacker@gmail.com>
2017-04-01 16:33:46 +0200

    loadMigrationsFrom

--- src/Providers/ModuleProvider.php
+++ src/Providers/ModuleProvider.php
@@ -24,13 +24,11 @@ class ModuleProvider extends ServiceProvider
 
         $this->loadViewsFrom(__DIR__.'/../resources/views/', 'objects');
         $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'objects');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
         $this->publishes([
             __DIR__.'/../resources/views' => base_path('resources/views/vendor/objects'),
         ], 'views');
-        $this->publishes([
-            __DIR__.'/../database' => base_path('database'),
-        ], 'migrations');
 
         AliasLoader::getInstance()->alias('Objects', Objects::class);
 
