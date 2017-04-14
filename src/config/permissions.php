12e08dd95de75550b7af3fad4c09e04b0c8fbc79
Samuel De Backer <sdebacker@gmail.com>
2017-04-14 13:27:27 +0200

    permissions in config

---
+++ src/config/permissions.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'objects' => [
+        'index-objects' => 'Show list',
+        'create-object' => 'Create',
+        'store-object' => 'Store',
+        'edit-object' => 'Edit',
+        'update-object' => 'Update',
+        'destroy-object' => 'Destroy',
+    ],
+];
