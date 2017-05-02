2633b9560166fc9c06960647aa13794bab334ac6
Samuel De Backer <sdebacker@gmail.com>
2017-05-02 18:18:29 +0200

    can have many files

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -47,6 +47,7 @@ class RouteServiceProvider extends ServiceProvider
                 $router->get('objects', 'AdminController@index')->name('admin::index-objects')->middleware('can:see-all-objects');
                 $router->get('objects/create', 'AdminController@create')->name('admin::create-object')->middleware('can:create-object');
                 $router->get('objects/{object}/edit', 'AdminController@edit')->name('admin::edit-object')->middleware('can:update-object');
+                $router->get('objects/{object}/files', 'AdminController@files')->name('admin::edit-object-files')->middleware('can:update-object');
                 $router->post('objects', 'AdminController@store')->name('admin::store-object')->middleware('can:create-object');
                 $router->put('objects/{object}', 'AdminController@update')->name('admin::update-object')->middleware('can:update-object');
                 $router->patch('objects/{ids}', 'AdminController@ajaxUpdate')->name('admin::update-object-ajax')->middleware('can:update-object');
