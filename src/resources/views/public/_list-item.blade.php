8f35c7f45a6296a0b92f5a1a94301991a127f8fa
Samuel De Backer <sdebacker@gmail.com>
2018-12-19 12:21:21 +0100

    ->uri() call in place of route() helper

--- src/resources/views/public/_list-item.blade.php
+++ src/resources/views/public/_list-item.blade.php
@@ -1,5 +1,5 @@
 <li class="objects-item">
-    <a class="objects-item-link" href="{{ route($lang.'::object', $object->slug) }}" title="{{ $object->title }}">
+    <a class="objects-item-link" href="{{ $object->uri() }}" title="{{ $object->title }}">
         <span class="objects-item-title">{!! $object->title !!}</span>
         <span class="objects-item-image">{!! $object->present()->thumb(null, 200) !!}</span>
     </a>
