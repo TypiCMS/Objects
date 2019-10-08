0677c38e357eae69e669edb1150b7f267b4ed209
Samuel De Backer <sdebacker@gmail.com>
2019-10-08 21:35:57 +0200

    Update index.blade.php

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -13,7 +13,7 @@
     include="image"
     appends="thumb"
     :searchable="['title']"
-    :sorting="['-title_translated']">
+    :sorting="['title_translated']">
 
     <template slot="add-button">
         @include('core::admin._button-create', ['module' => 'objects'])
