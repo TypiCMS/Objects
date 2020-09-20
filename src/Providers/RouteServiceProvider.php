6d016ea540f42d58ce1b0beaa0471e2a8c36f0de
Samuel De Backer <sdebacker@gmail.com>
2020-09-20 22:39:31 +0200

    Routes definition cleaned

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -17,43 +17,39 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map()
     {
-        Route::namespace($this->namespace)->group(function (Router $router) {
-            /*
-             * Front office routes
-             */
-            if ($page = TypiCMS::getPageLinkedToModule('objects')) {
-                $middleware = $page->private ? ['public', 'auth'] : ['public'];
-                $router->middleware($middleware)->group(function (Router $router) use ($page) {
-                    foreach (locales() as $lang) {
-                        if ($page->translate('status', $lang) && $uri = $page->uri($lang)) {
-                            $router->get($uri, [PublicController::class, 'index'])->name($lang.'::index-objects');
-                            $router->get($uri.'/{slug}', [PublicController::class, 'show'])->name($lang.'::object');
-                        }
-                    }
-                });
+        /*
+         * Front office routes
+         */
+        if ($page = TypiCMS::getPageLinkedToModule('objects')) {
+            $middleware = $page->private ? ['public', 'auth'] : ['public'];
+            foreach (locales() as $lang) {
+                if ($page->isPublished($lang) && $uri = $page->uri($lang)) {
+                    Route::middleware($middleware)->prefix($uri)->name($lang.'::')->group(function (Router $router) {
+                        $router->get('/', [PublicController::class, 'index'])->name('index-objects');
+                        $router->get('{slug}', [PublicController::class, 'show'])->name('object');
+                    });
+                }
             }
+        }
 
-            /*
-             * Admin routes
-             */
-            $router->middleware('admin')->prefix('admin')->group(function (Router $router) {
-                $router->get('objects', [AdminController::class, 'index'])->name('admin::index-objects')->middleware('can:read objects');
-                $router->get('objects/create', [AdminController::class, 'create'])->name('admin::create-object')->middleware('can:create objects');
-                $router->get('objects/{object}/edit', [AdminController::class, 'edit'])->name('admin::edit-object')->middleware('can:read objects');
-                $router->post('objects', [AdminController::class, 'store'])->name('admin::store-object')->middleware('can:create objects');
-                $router->put('objects/{object}', [AdminController::class, 'update'])->name('admin::update-object')->middleware('can:update objects');
-            });
+        /*
+         * Admin routes
+         */
+        Route::middleware('admin')->prefix('admin')->name('admin::')->group(function (Router $router) {
+            $router->get('objects', [AdminController::class, 'index'])->name('index-objects')->middleware('can:read objects');
+            $router->get('objects/create', [AdminController::class, 'create'])->name('create-object')->middleware('can:create objects');
+            $router->get('objects/{object}/edit', [AdminController::class, 'edit'])->name('edit-object')->middleware('can:read objects');
+            $router->post('objects', [AdminController::class, 'store'])->name('store-object')->middleware('can:create objects');
+            $router->put('objects/{object}', [AdminController::class, 'update'])->name('update-object')->middleware('can:update objects');
+        });
 
-            /*
-             * API routes
-             */
-            $router->middleware('api')->prefix('api')->group(function (Router $router) {
-                $router->middleware('auth:api')->group(function (Router $router) {
-                    $router->get('objects', [ApiController::class, 'index'])->middleware('can:read objects');
-                    $router->patch('objects/{object}', [ApiController::class, 'updatePartial'])->middleware('can:update objects');
-                    $router->delete('objects/{object}', [ApiController::class, 'destroy'])->middleware('can:delete objects');
-                });
-            });
+        /*
+         * API routes
+         */
+        Route::middleware(['api', 'auth:api'])->prefix('api')->group(function (Router $router) {
+            $router->get('objects', [ApiController::class, 'index'])->middleware('can:read objects');
+            $router->patch('objects/{object}', [ApiController::class, 'updatePartial'])->middleware('can:update objects');
+            $router->delete('objects/{object}', [ApiController::class, 'destroy'])->middleware('can:delete objects');
         });
     }
 }
