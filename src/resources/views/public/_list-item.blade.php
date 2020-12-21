f1a188a8267ef753495493c226cbec93248229a7
Samuel De Backer <sdebacker@gmail.com>
2020-12-21 11:10:05 +0100

    Check empty image + missing alt attribute.

--- src/resources/views/public/_list-item.blade.php
+++ src/resources/views/public/_list-item.blade.php
@@ -1,8 +1,10 @@
 <li class="object-list-item">
     <a class="object-list-item-link" href="{{ $object->uri() }}" title="{{ $object->title }}">
-        <span class="object-list-item-title">{{ $object->title }}</span>
-        <span class="object-list-item-image-wrapper">
-            <img class="object-list-item-image" src="{{ $object->present()->image(null, 200) }}" width="{{ $object->image->width }}" height="{{ $object->image->height }}" alt="">
-        </span>
+        <div class="object-list-item-title">{{ $object->title }}</div>
+        <div class="object-list-item-image-wrapper">
+            @empty (!$object->image)
+            <img class="object-list-item-image" src="{{ $object->present()->image(null, 200) }}" width="{{ $object->image->width }}" height="{{ $object->image->height }}" alt="{{ $object->image->alt_attribute }}">
+            @endempty
+        </div>
     </a>
 </li>
