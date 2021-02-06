7751363fe9105133efbc4222bc29a1b2adec0b8d
Samuel De Backer <sdebacker@gmail.com>
2021-02-06 20:06:25 +0100

    title html tags in admin lists

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -33,7 +33,7 @@
         <td v-if="$can('update objects')">@include('core::admin._button-edit', ['module' => 'objects'])</td>
         <td><item-list-status-button :model="model"></item-list-status-button></td>
         <td><img :src="model.thumb" alt="" height="27"></td>
-        <td>@{{ model.title_translated }}</td>
+        <td v-html="model.title_translated"></td>
     </template>
 
 </item-list>
