c37e68c07b389b7c391d72d46cff5382d5a91459
Samuel De Backer <sdebacker@gmail.com>
2022-04-19 21:35:27 +0200

    forms

--- src/resources/views/admin/_form.blade.php
+++ src/resources/views/admin/_form.blade.php
@@ -3,19 +3,27 @@
     <script src="{{ asset('components/ckeditor4/config-full.js') }}"></script>
 @endpush
 
-@component('core::admin._buttons-form', ['model' => $model])
-@endcomponent
-
-{!! BootForm::hidden('id') !!}
-
-<file-manager related-table="{{ $model->getTable() }}" :related-id="{{ $model->id ?? 0 }}"></file-manager>
-<file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
-<files-field :init-files="{{ $model->files }}"></files-field>
-
-@include('core::form._title-and-slug')
-<div class="mb-3">
-    {!! TranslatableBootForm::hidden('status')->value(0) !!}
-    {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
+<div class="header">
+    @include('core::admin._button-back', ['url' => $model->indexUrl(), 'title' => __('Objects')])
+    @include('core::admin._title', ['default' => __('New object')])
+    @component('core::admin._buttons-form', ['model' => $model])
+    @endcomponent
 </div>
-{!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
-{!! TranslatableBootForm::textarea(__('Body'), 'body')->addClass('ckeditor-full') !!}
+
+<div class="content">
+
+    {!! BootForm::hidden('id') !!}
+
+    <file-manager related-table="{{ $model->getTable() }}" :related-id="{{ $model->id ?? 0 }}"></file-manager>
+    <file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
+    <files-field :init-files="{{ $model->files }}"></files-field>
+
+    @include('core::form._title-and-slug')
+    <div class="mb-3">
+        {!! TranslatableBootForm::hidden('status')->value(0) !!}
+        {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
+    </div>
+    {!! TranslatableBootForm::textarea(__('Summary'), 'summary')->rows(4) !!}
+    {!! TranslatableBootForm::textarea(__('Body'), 'body')->addClass('ckeditor-full') !!}
+
+</div>
--- src/resources/views/admin/create.blade.php
+++ src/resources/views/admin/create.blade.php
@@ -4,11 +4,6 @@
 
 @section('content')
 
-    <div class="header">
-        @include('core::admin._button-back', ['module' => 'objects'])
-        <h1 class="header-title">@lang('New object')</h1>
-    </div>
-
     {!! BootForm::open()->action(route('admin::index-objects'))->multipart()->role('form') !!}
         @include('objects::admin._form')
     {!! BootForm::close() !!}
